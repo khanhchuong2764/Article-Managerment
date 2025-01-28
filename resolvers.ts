5623ef4b294c4a3a196530d653f4c2ed6a3e6656
khanhchuong2764 <khanhchuong2764@gmail.com>
2025-01-28 23:17:10 +0700

    Quan ly bai viet

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,50 @@
+import { uptime } from "process";
+import Article from "./models/article.model"
+
 export const resolvers  = {
     Query: {
-      hello: () => {
-        return "Hello World"
+      getListArticle: async () => {
+        const articles = await Article.find({
+          deleted:false
+        }) 
+        return articles;
+      },
+      getArticle: async (_,args) => {
+        const {id} = args;
+        const article = await Article.findOne({
+          _id : id,
+          deleted:false
+        })
+        return article;
+      },
+    },
+    Mutation: {
+      createArticle: async (_,args) => {
+        const {article} = args;
+        const record = new Article(article);
+        await record.save();
+        return {
+          id:record.id,
+          title: record.title,
+          avatar: record.avatar,
+          description: record.description,
+          code:200,
+          message: "Success"
+        };
+      },
+      deleteArticle: async (_,args) => {
+        const {id} = args;
+        await Article.updateOne({_id:id,deleted:false},{deleted:true});
+        return {
+          code:200,
+          message: "Thành Công"
+        };
+      },
+      updateArticle: async (_,args) => {
+        const {article,id} = args;
+        await Article.updateOne({_id:id,deleted:false},article);
+        const record = await Article.findOne({_id : id,deleted:false});
+        return record;
       }
     }
   }
--- typeDefs.ts
+++ typeDefs.ts
@@ -1,7 +1,31 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
+    type Article{
+      id:String,
+      title:String,
+      avatar: String,
+      description:String,
+      code: Int,
+      message: String
+    }
+    
     type Query {
-      hello: String
+      getListArticle: [Article]
+      getArticle(id:ID): Article
+    }
+    input ArticleInput {
+      title:String,
+      avatar: String,
+      description:String
+    }
+    type ResponseCode {
+      code: Int,
+      message:String
+    }
+    type Mutation {
+      createArticle(article:ArticleInput): Article
+      deleteArticle(id:ID): ResponseCode
+      updateArticle(id:ID,article:ArticleInput): Article
     }
   `
